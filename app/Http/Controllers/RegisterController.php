01de3152c2bf91579b594391f4d6ac6c1b1111bd
Darko <darkomarjanovickop@gmail.com>
2022-12-17 06:01:04 +0100

    +register

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+    }
+
+    public function store()
+    {   //https://laravel.com/docs/9.x/validation
+        $attributes = request()->validate([
+            'name' => ['required', 'max:255'],
+            'username' => ['required', 'max:255', 'min:3', Rule::unique('users', 'username')],
+            'email' => ['required', 'max:255', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:7', 'max:255']
+        ]);
+
+        User::create($attributes);
+    }
+}
