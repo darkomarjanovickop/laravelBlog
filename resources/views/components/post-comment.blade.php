121588b1369e8280932f01ea610f099f0a56cef1
Darko <darkomarjanovickop@gmail.com>
2022-12-18 08:32:14 +0100

    +static comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,16 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+
+            <p class="text-sm">Posted <time>8 months ago</time></p>
+        
+        </header>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis magni ex culpa harum corporis tempora laudantium dignissimos veritatis? Eligendi voluptas tempora natus unde dolor.
+        </p>
+    </div>
+</article>
