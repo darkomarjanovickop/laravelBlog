518f6861e59576ec2f48dd58a4d554e2748604fa
Darko <darkomarjanovickop@gmail.com>
2022-12-19 18:51:01 +0100

    +comment form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,13 +22,13 @@
                 <div class="col-span-8">
                     <div class="hidden lg:flex justify-between mb-6">
                         <a href="/"
-                           class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
+                            class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
                             <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
                                 <g fill="none" fill-rule="evenodd">
                                     <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
                                     </path>
                                     <path class="fill-current"
-                                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
+                                        d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
                                     </path>
                                 </g>
                             </svg>
@@ -48,8 +48,29 @@
                     <div class="space-y-4 lg:text-lg leading-loose">{!! $post->body !!}</div>
                 </div>
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-panel>
+                        <form action="#" method="post">
+                            @csrf
+                            <header class="flex intems-center">
+                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40"
+                                    height="40" class="rounded-full">
+                                <h2 class="ml-4">Want to participate?</h2>
+                            </header>
+    
+                            <div class="mt-6">
+                                <textarea name="body" rows="5" class="w-full text-sm focus:outline-none focus:ring"
+                                    placeholder="Quick! Think of something to say!"></textarea>
+                            </div>
+                            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                                <button type="submit"
+                                    class="bg-blue-500 text-white uppercase font-semibold text-sm py-2 px-10 rouded-2xl hover:bg-blue-600">
+                                    POST</button>
+                            </div>
+                        </form>
+                    </x-panel>
+
                     @foreach ($post->comments as $comment)
-                    <x-post-comment :comment="$comment"/>
+                        <x-post-comment :comment="$comment" />
                     @endforeach
 
                 </section>
